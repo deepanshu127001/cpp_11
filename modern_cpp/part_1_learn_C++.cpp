c04de0275ff8d784451fc0a7ac5f0adc946f6238
deepanshu127001 <deepanshu127001@gmail.com>
2022-12-28 03:48:08 +0530

    moder ++

---
+++ modern_cpp/part_1_learn_C++.cpp
@@ -0,0 +1,25 @@
+//introduction
+//Initializer list
+#include <iostream>
+#include <initializer_list>
+#include <vector>
+class vBool{
+    std::vector<int> m_vector;
+    public:
+    vBool(const std::initializer_list<int> &v){
+        for(std::initializer_list<int>::iterator itr;itr!=v.end();itr++){
+            m_vector.push_back(*itr);
+        }
+    }
+};
+int main(){
+  int arr[]={5,4,3,2,1};
+  for(int val : arr){
+    std::cout<<val<<" ";
+  }
+  //initializer_list was introduced in c++ 11 before this we can only insert values using push_back() method
+ std::vector <int> v={1,2,3,4};//calling initializer list consructor
+  for(int val : v){
+    std::cout<<val<<" ";
+  }
+}
